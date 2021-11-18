c19b0dbeea08f3c66bbb414a5138a4180fba91a0
Dinesh Kumar <dkdinesh6688@gmail.com>
2021-11-18 22:41:29 +0530

    ecommerce

--- Modules/Login/controller.js
+++ Modules/Login/controller.js
@@ -9,9 +9,11 @@ const {
 const Register = mongoose.model("user_register");
 
 exports.user_login = async (req, res, next) => {
-
   var token;
-  Register.find({ user_mail: req.body.user_mail, user_role: req.body.user_role })
+  Register.find({
+    user_mail: req.body.user_mail,
+    user_role: req.body.user_role,
+  })
     .exec()
     .then(async (user) => {
       if (user.length < 1) {
@@ -45,49 +47,6 @@ exports.user_login = async (req, res, next) => {
             }
           }
         );
-      }
-    });
-};
-
-exports.user_logout = (req, res) => {
-  var date = moment().format("LL");
-  var timing = moment().format("LLL");
-  Signup.find({ emp_mail: req.body.emp_mail })
-    .exec()
-    .then(async (user) => {
-      if (user.length < 1) {
-        res.status(401).json({
-          success: false,
-          message: "User does not exist!",
-        });
-      } else {
-        if (user[0].role != 0) {
-          Signup.find({
-            attendence: { $elemMatch: { date: date } },
-          }).then((present) => {
-            if (present.length > 0) {
-              Signup.findOneAndUpdate(
-                { emp_mail: req.body.emp_mail, "attendence.date": date },
-                { $set: { "attendence.$.loggedOut": timing } }
-              ).then(() => {
-                res.status(200).json({
-                  success: true,
-                  message: "Successfully logged out!!",
-                });
-              });
-            } else {
-              res.status(200).json({
-                success: true,
-                message: "Not logged in this date!!",
-              });
-            }
-          });
-        } else {
-          res.status(200).json({
-            success: true,
-            message: "Successfully logged out!!",
-          });
-        }
       }
     });
 };
